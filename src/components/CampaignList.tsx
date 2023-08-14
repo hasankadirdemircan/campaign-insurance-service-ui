64f9e9efdc21e7774a26b7041049135f4291b2fd
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2023-08-15 02:25:03 +0300

    updated state,store and extracter thunk

--- src/components/CampaignList.tsx
+++ src/components/CampaignList.tsx
@@ -2,34 +2,36 @@
 
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchCampaigns } from '../api/campaignApi'
-import { AppDispatch, RootState } from '../app/store';
+import { fetchCampaigns } from '../api/campaignApi';
+import { AppDispatch } from '../app/store'; 
+import { RootState }  from "../app/types"
 
 const CampaignList: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
-  const campaigns = useSelector((state: RootState) => state.campaigns.campaigns);
-
+  
+  const campaigns = useSelector((state: RootState) => state.campaign.campaigns);
+  
   useEffect(() => {
     dispatch(fetchCampaigns());
   }, [dispatch]);
 
   return (
     <div>
-    <h1>Campaign List</h1>
-    <ul>
-      {campaigns.map((item) => {
-        const campaign = item.campaign;
-        return (
-          <li key={campaign.id}>
-            <h2>{campaign.campaignTitle}</h2>
-            <p>{campaign.campaignDetail}</p>
-            <p>Category: {campaign.campaignCategory}</p>
-            <p>Status: {campaign.campaignStatus}</p>
-          </li>
-        );
-      })}
-    </ul>
-  </div>
+      <h1>Campaign List</h1>
+      <ul>
+        {campaigns.map((item) => {
+          const campaign = item.campaign;
+          return (
+            <li key={campaign.id}>
+              <h2>{campaign.campaignTitle}</h2>
+              <p>{campaign.campaignDetail}</p>
+              <p>Category: {campaign.campaignCategory}</p>
+              <p>Status: {campaign.campaignStatus}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
