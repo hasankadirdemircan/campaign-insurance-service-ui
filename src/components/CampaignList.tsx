78a3cbc6cfc1322566ae3f0d18c37edbe70dff51
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2023-08-18 21:24:42 +0300

    loading animation
    added loading animation in CampaignList.tsx

--- src/components/CampaignList.tsx
+++ src/components/CampaignList.tsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { deleteCampaign, fetchCampaigns } from '../api/campaignApi';
 import { AppDispatch } from '../app/store';
 import { RootState } from "../app/types";
-import { Button, Container, Modal, Table, Toast, ToastContainer } from 'react-bootstrap';
+import { Button, Container, Modal, Spinner, Table, Toast, ToastContainer } from 'react-bootstrap';
 import './CampaignList.css';
 import { Campaign } from '../features/campaign/models/campaign';
 
@@ -11,10 +11,10 @@ const CampaignList: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
 
   const campaigns = useSelector((state: RootState) => state.campaign.campaigns);
+  const loading = useSelector((state: RootState) => state.campaign.loading); // Redux store'dan loading durumunu alın
 
   const [showToast, setShowToast] = useState(false);
   const [selectedCampaign, setSelectedCampaign] = useState<Campaign | null>(null);
-
   const [showConfirmation, setShowConfirmation] = useState(false);
 
   const handleDeleteClick = (campaign: Campaign) => {
@@ -39,63 +39,74 @@ const CampaignList: React.FC = () => {
   }, [dispatch]);
 
   return (
-    <Container className='mt-4'>
-      <Table responsive hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Title</th>
-            <th>Detail</th>
-            <th>Category</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {campaigns.map((item, index) => {
-            const campaign = item.campaign;
-            return (
-              <tr key={campaign.id}>
-                <td>{index + 1}</td>
-                <td>{campaign.campaignTitle}</td>
-                <td>{campaign.campaignDetail}</td>
-                <td>{campaign.campaignCategory}</td>
-                <td>{campaign.campaignStatus}</td>
-                <td>
-                  <Button variant='outline-danger' size='sm' onClick={() => handleDeleteClick(campaign)}> DELETE </Button>
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </Table>
+    <div className="campaign-list-container">
+      {loading && (
+        <div className="loading-overlay">
+          <Spinner animation="border" role="status" variant="light">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      )}
 
-      <Modal show={showConfirmation} onHide={() => setShowConfirmation(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Confirm Deletion</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          Are you sure you want to delete the campaign "{selectedCampaign?.campaignTitle}"?
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant='secondary' onClick={() => setShowConfirmation(false)}>Cancel</Button>
-          <Button variant='danger' onClick={handleDeleteConfirmed}>Delete</Button>
-        </Modal.Footer>
-      </Modal>
+      <Container className={`mt-4 ${loading ? 'blur' : ''}`}>
 
-      <ToastContainer position='top-end'>
-        <Toast show={showToast} onClose={() => setShowToast(false)} className={`fade-out-toast ${selectedCampaign ? 'bg-success text-white' : ''}`}>
-          <Toast.Header closeButton={false}>
-            <strong className='me-auto'>Notification</strong>
-            <button type="button" className="btn-close" aria-label="Close" onClick={() => setShowToast(false)}></button>
-          </Toast.Header>
-          <Toast.Body className={selectedCampaign ? 'bg-success text-white' : ''}>
-            {selectedCampaign ? `Campaign '${selectedCampaign.campaignTitle}' has been deleted.` : ''}
-          </Toast.Body>
-        </Toast>
-      </ToastContainer>
+        <Table responsive hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Detail</th>
+              <th>Category</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {campaigns.map((item, index) => {
+              const campaign = item.campaign;
+              return (
+                <tr key={campaign.id}>
+                  <td>{index + 1}</td>
+                  <td>{campaign.campaignTitle}</td>
+                  <td>{campaign.campaignDetail}</td>
+                  <td>{campaign.campaignCategory}</td>
+                  <td>{campaign.campaignStatus}</td>
+                  <td>
+                    <Button variant='outline-danger' size='sm' onClick={() => handleDeleteClick(campaign)}> DELETE </Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
 
-    </Container>
 
+        <Modal show={showConfirmation} onHide={() => setShowConfirmation(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Confirm Deletion</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Are you sure you want to delete the campaign "{selectedCampaign?.campaignTitle}"?
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant='secondary' onClick={() => setShowConfirmation(false)}>Cancel</Button>
+            <Button variant='danger' onClick={handleDeleteConfirmed}>Delete</Button>
+          </Modal.Footer>
+        </Modal>
+
+        <ToastContainer position='top-end'>
+          <Toast show={showToast} onClose={() => setShowToast(false)} className={`fade-out-toast ${selectedCampaign ? 'bg-success text-white' : ''}`}>
+            <Toast.Header closeButton={false}>
+              <strong className='me-auto'>Notification</strong>
+              <button type="button" className="btn-close" aria-label="Close" onClick={() => setShowToast(false)}></button>
+            </Toast.Header>
+            <Toast.Body className={selectedCampaign ? 'bg-success text-white' : ''}>
+              {selectedCampaign ? `Campaign '${selectedCampaign.campaignTitle}' has been deleted.` : ''}
+            </Toast.Body>
+          </Toast>
+        </ToastContainer>
+
+      </Container>
+    </div>
   );
 };
 
--- src/features/campaign/campaignSlice.ts
+++ src/features/campaign/campaignSlice.ts
@@ -6,10 +6,13 @@ import { deleteCampaign, fetchCampaigns } from '../../api/campaignApi'; // Yeni 
 
 export interface CampaignState {
   campaigns: CampaignData[];
+  loading: boolean; // Loading durumu
+
 }
 
 const initialState: CampaignState = {
   campaigns: [],
+  loading: false
 };
 
 const campaignSlice = createSlice({
@@ -19,12 +22,21 @@ const campaignSlice = createSlice({
 
   },
   extraReducers: (builder) => {
+    builder.addCase(fetchCampaigns.pending, (state) => {
+      state.loading = true;
+    });
     builder.addCase(fetchCampaigns.fulfilled, (state, action) => {
       state.campaigns = action.payload;
+      state.loading = false; // Yükleme tamamlandı, loading durumunu false yap
+
+    });
+    builder.addCase(deleteCampaign.pending, (state) => {
+      state.loading = true; // Silme işlemi başlatıldığında loading durumu true yap
     });
     builder.addCase(deleteCampaign.fulfilled, (state, action) => {
       const deletedCampaignId = action.meta.arg; // Silinen kampanyanın kimliği
       state.campaigns = state.campaigns.filter(campaign => campaign.campaign.id !== deletedCampaignId);
+      state.loading = false;
     });
   },
 });
